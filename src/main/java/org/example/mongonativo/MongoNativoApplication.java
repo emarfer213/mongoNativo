e8d2f07f5bc006caea8c0007c860bca14474652c
emarfer213 <emiliomartinf2002@gmail.com>
2026-02-07 19:03:35 +0100

    tarea completada

--- src/main/java/org/example/mongonativo/MongoNativoApplication.java
+++ src/main/java/org/example/mongonativo/MongoNativoApplication.java
@@ -35,6 +35,10 @@ public class MongoNativoApplication {
         }
     }
 
+    /**
+     * metodo auxiliar que imprime por pantalla
+     * un menu con las opciones disponibles
+     */
     private static void menu() {
         System.out.println("""
                 ---------MENU------------
@@ -46,6 +50,10 @@ public class MongoNativoApplication {
                 """);
     }
 
+    /**
+     * metodo auxiliar que utiliza un bucle para mantener el menu activo
+     * y funcional hasta que la opcion de salir sea seleccionada
+     */
     private static void bucleMenu() {
         boolean exit = false;
         int opcion = -1;
@@ -60,15 +68,19 @@ public class MongoNativoApplication {
             }
             switch (opcion) {
                 case 1:
-                    crearDatosPredefinidos(clientesCollection, juegosCollection, ventaCollection);
+                    //llamada al metodo que introducira datos predeterminados a la base de datos
+                    crearDatosPredefinidos();
                     break;
                 case 2:
+                    //llamada al metodo que realizara una venta
                     procesarVenta("email1@gmail.com", "juego2");
                     break;
                 case 3:
+                    //llamda al metodo que mostrara todas las compras que un cliente haya hecho
                     historialDeCliente("email1@gmail.com");
                     break;
                 case 4:
+                    //llamda al metodo que mostrar por pantalla todos los juegos en oferta
                     listaDeOfertas();
                     break;
                 case 5:
@@ -81,6 +93,13 @@ public class MongoNativoApplication {
         }
     }
 
+    /**
+     * metodo el cual creara un objeto cliente con los datos introducidos por parametros
+     * este comprobara primero si los datos son correctos, si no lo son enviara un mensaje de error
+     * @param nombre
+     * @param email
+     * @return
+     */
     private static Document crearCliente(String nombre, String email) {
         if (email == null || !email.contains("@")) {
             throw new IllegalArgumentException(
@@ -93,34 +112,36 @@ public class MongoNativoApplication {
                 .append("fecha_registro", LocalDate.now());
     }
 
+    /**
+     * metodo el cual crea un objeto videojuego, recibiendo por parametros sus datos,
+     * ademas comprueba que el precio ni el stock sean menores a 0,
+     * si es igual o menor a 0 mandara una excepcion
+     * @param titulo
+     * @param genero
+     * @param precio
+     * @param stock
+     * @return
+     */
     private static Document crearJuego(String titulo, String genero, double precio, int stock) {
-        if (precio <= 0) {
+        if (precio <= 0 || stock <= 0) {
             throw new IllegalArgumentException(
-                    "El precio del producto no puede ser menor o igua a 0"
+                    "El precio del producto o el valor del stock no puede ser menor o igua a 0"
             );
         }
-        if (stock <= 0) {
-            throw new IllegalArgumentException(
-                    "La cantidad en stock de un producto no puede ser 0 o menor"
-            );
-        }
+
         return new Document("titulo", titulo)
                 .append("genero", genero)
                 .append("precio", precio)
                 .append("stock", stock);
     }
 
-    private static void crearDatosPredefinidos(MongoCollection<Document> clientesCollection, MongoCollection<Document> juegosCollection, MongoCollection<Document> ventaCollection) {
-        //limpieza
-        clientesCollection.drop();
-        juegosCollection.drop();
-
-        //insercion
-        extraerClientesValidos(clientesCollection);
-        extraerJuegosValidos(juegosCollection);
-    }
-
-    private static void extraerJuegosValidos(MongoCollection<Document> juegosCollection) {
+    /**
+     * metodo el cual recogera en una lista todos los objetos juego
+     * que queramos crear y que tengan valores validos, mediante un bucle recorrera una matriz
+     * de la cual sacara los datos, si alguno es incorrecto ese juego no se creara
+     * y pasara al siguiente conjunto de datos
+     */
+    private static void extraerJuegosValidos() {
         List<Document> juegosValidos = new ArrayList<>();
 
         String[][] datosJuegos = {
@@ -135,10 +156,10 @@ public class MongoNativoApplication {
             try {
                 juegosValidos.add(
                         crearJuego(
-                                datos[0],
-                                datos[1],
-                                Double.parseDouble(datos[2]),
-                                Integer.parseInt(datos[3])
+                                datos[0], //titulo
+                                datos[1], //genero
+                                Double.parseDouble(datos[2]), //precio
+                                Integer.parseInt(datos[3]) //stock
                         )
                 );
             } catch (IllegalArgumentException e) {
@@ -146,12 +167,19 @@ public class MongoNativoApplication {
             }
         }
 
+        //si la lista final no esta vacia, guardara todos los juegos
         if (!juegosValidos.isEmpty()) {
             juegosCollection.insertMany(juegosValidos);
         }
     }
 
-    private static void extraerClientesValidos(MongoCollection<Document> clientesCollection) {
+    /**
+     * metodo el cual recogera en una lista todos los objetos cliente
+     * que queramos crear y que tengan valores validos, mediante un bucle recorrera una matriz
+     * de la cual sacara los datos, si alguno es incorrecto ese cliente no se creara
+     * y pasara al siguiente conjunto de datos
+     */
+    private static void extraerClientesValidos() {
         List<Document> clientesValidos = new ArrayList<>();
 
         List<String[]> datosClientes = Arrays.asList(
@@ -167,37 +195,67 @@ public class MongoNativoApplication {
             }
         }
 
+        //si la lista resultante no esta vacia se guardaran los datos
         if (!clientesValidos.isEmpty()) {
             clientesCollection.insertMany(clientesValidos);
         }
     }
 
-    private static void procesarVenta(String emailCliente, String tituloJuego){
+    /**
+     * metodo que borrara todos los datos de las tablas pre existentes
+     * y carganara los datos nuevos llamando a los metodos auxiliares
+     * definidos preciamente
+     */
+    private static void crearDatosPredefinidos() {
+        clientesCollection.drop();
+        juegosCollection.drop();
+
+        extraerClientesValidos();
+        extraerJuegosValidos();
+    }
+
+    /**
+     * metodo el cual realizara toda la logica de procesar la venta,
+     * comprobara que no tenga datos incorrectos, utilizaremos dos objetos para guardar los datos
+     * del juego y cliente que tengan los datos metidos por parametro
+     * @param emailCliente
+     * @param tituloJuego
+     */
+    private static void procesarVenta(String emailCliente, String tituloJuego) {
+
+        //crearemos dos objetos que guarden los datos del cliente y videojuego correspondiente
         Document cliente = clientesCollection.find(eq("email", emailCliente)).first();
-        if (cliente == null){
+        Document juego = juegosCollection.find(eq("titulo", tituloJuego)).first();
+
+        //comprobamos que el cliente exista
+        if (cliente == null) {
             System.out.println("cliente no encontrado");
             return;
         }
 
-        Document juego = juegosCollection.find(eq("titulo", tituloJuego)).first();
-        if (juego == null){
+        //comprobamos que el juego existe
+        if (juego == null) {
             System.out.println("juego no encontrado");
             return;
         }
 
-        if (juego.getInteger("stock") <= 0){
+        //comprobamos que el stock no sea cero o menor
+        if (juego.getInteger("stock") <= 0) {
             System.out.println("stock del juego agotado");
             return;
         }
 
+        //creamos un objeto venta con los datos correspondientes
         Document venta = new Document("fecha", LocalDate.now())
                 .append("cliente_id", cliente.getObjectId("_id"))
                 .append("juego_id", juego.getObjectId("_id"))
                 .append("titulo_snapshot", juego.getString("titulo"))
                 .append("precio_snapshot", juego.getDouble("precio"));
 
+        //guardamos en la base de datos la venta
         ventaCollection.insertOne(venta);
 
+        //actualizamos el stock del juego restandole 1
         juegosCollection.updateOne(
                 eq("_id", juego.getObjectId("_id")),
                 inc("stock", -1)
@@ -206,19 +264,31 @@ public class MongoNativoApplication {
         System.out.println("Venta realizada correctamente");
     }
 
-    private static void historialDeCliente(String emailCliente){
+    /**
+     * metodo que usaremos para mostrar por pantalla todas las compras
+     * que un cliente ha realizado, sacaremos la informacion del cliente que contenga
+     * el email pasado por parametro e imprimiremos por pantalla el nombre del juego y el precio
+     * @param emailCliente
+     */
+    private static void historialDeCliente(String emailCliente) {
+        //creamos un objeto cliente con los datos del cliente propietario del email
         Document cliente = clientesCollection.find(eq("email", emailCliente)).first();
-        if (cliente == null){
+
+        //comprobamos que el usuario exista, si no lo hace la operacion no se realiza
+        if (cliente == null) {
             System.out.println("cliente no encontrado");
             return;
         }
 
+        //sacamos la id del cliente para utilizarla  para buscar las ventas que contengan dicha id
         ObjectId clienteID = cliente.getObjectId("_id");
 
+        //recogemos todas las compras que contengan dicho id de cliente
         FindIterable<Document> compras = ventaCollection.find(
                 eq("cliente_id", clienteID)
         );
 
+        //imprimiremos por pantalla todas y cada una de dichas ventas
         System.out.println("Compras de " + emailCliente + ":");
         for (Document v : compras) {
             System.out.println("Titulo: " + v.getString("titulo_snapshot")
@@ -226,10 +296,15 @@ public class MongoNativoApplication {
         }
     }
 
-    private static void listaDeOfertas(){
+    /**
+     * clase la cual listara todos los videojuegos con un precio menor de 25
+     * ademas solamente mostrara el titulo y precio del videojuego,
+     * excluyendo su id
+     */
+    private static void listaDeOfertas() {
         System.out.println("Los juegos que se encuentran en oferta son: ");
         juegosCollection.find(lt("precio", 25.00))
-                .projection(Projections.fields(include("titulo", "precio"),exclude("_id")))
+                .projection(Projections.fields(include("titulo", "precio"), exclude("_id")))
                 .forEach(doc -> System.out.println("\n" + doc.toJson()));
     }
 }
